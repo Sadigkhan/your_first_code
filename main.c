c09e7a888cbebe5e985710efc812d8ae875771b6
Sadigkhan Gaykhanov <90463823+Sadigkhan@users.noreply.github.com>
2024-05-26 14:47:20 +0400

    Again and again

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 33; i++)
-  {
-    printf("Hello School\n");
-  }
-  return 0;
+
+int main() {
+    int i;
+    for (i = 0; i < 32; i++) {
+        printf("Hello School\n");
+    }
+    return 0;
 }
